1b3e719b230f0f82ad0dd9532f93c2ee6d55a463
Ubuntu <ubuntu@ip-172-31-30-32.eu-west-1.compute.internal>
2025-07-30 09:17:29 +0000

    added first.sh

---
+++ first.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo " HI ther i am learning SHell Scropt"
+
+echo " #!/bin/bash is type of my script" 
+
+echo " know i will create a folder"
+
+mkdir -p firstfolder
+
+echo " created folder"
+
+echo "hi i have file inside vim by writing shellscript" > firstfile.txt
+
+
